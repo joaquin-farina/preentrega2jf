4d0b9777e7b9fc7115d6a912abf3af23cb29fe06
Joaquin Fariña <joaquinramiro98@gmail.com>
2023-09-02 05:48:38 -0300

    tercera entrega

--- main.js
+++ main.js
@@ -1,104 +1,96 @@
-alert("Bienvenido a Mundo Calzado"); 
-
-class Zapatillas { 
-    constructor(marca, modelo, precio) {
-        this.marca = marca;
-        this.modelo = modelo;
+class Producto {
+    constructor(nombre, precio, imagen) {
+        this.nombre = nombre;
         this.precio = precio;
+        this.imagen = imagen;
     }
-};
-
-const Calzado = () => {
-    const calzadoCargado = [];
-    calzadoCargado.push(new Zapatillas("Adidas", "Adizero", 50000));
-    calzadoCargado.push(new Zapatillas("Adidas", "Superstar", 38000));
-    calzadoCargado.push(new Zapatillas("Adidas", "Yeezy", 100000));
-    calzadoCargado.push(new Zapatillas("Nike", "Air", 73000));
-    calzadoCargado.push(new Zapatillas("Nike", "Jordan", 97000));
-    calzadoCargado.push(new Zapatillas("Nike", "Kobe", 240000));
-    return calzadoCargado;
-};
-
-const compraOVenta = () => { 
-    let venderOComprar = prompt("Quieres vender o comprar zapatillas?");
-    if (venderOComprar.toLowerCase() === "comprar") {
-        let Eleccion = Comprar(); 
-        cuponDescuento(Eleccion); 
-    } else if (venderOComprar.toLowerCase() === "vender") {
-        let calzadoPreCargado = Calzado(); 
-        let calzadoCargadoPorElUsuario = venderCalzado(); 
-        concatDeCalzado(calzadoPreCargado, calzadoCargadoPorElUsuario); 
-    } else alert("Opcion invalida");
-};
-
-const Comprar = () => {
-    let comprar = prompt("Elija marca y modelo de la zapatilla"); 
-    let calzadoCargado = Calzado();
-    let calzadoAComprar = [];
-    const filtro = calzadoCargado.filter(item => { 
-        return item.marca.toLowerCase() === comprar.toLowerCase();
-    });
-
-    if (filtro.length === 0) { 
-        alert(`No tenemos stock de esa marca ${comprar}`);
-        const marcas = [...new Set(calzadoCargado.map(calzado => calzado.marca))];
-        let marcasCadena = marcas.join(", "); 
-        let mensaje = `Tenemos zapatillas de estas marcas ${marcasCadena}`;
-        alert(mensaje);
-        Comprar();
-    };
-
-    filtro.forEach((item, index) => { 
-        let mensaje = `
-        Zapatilla: ${index + 1}
-        Marca: ${item.marca}
-        Modelo: ${item.modelo}
-        Precio: $${item.precio}`;
-        calzadoAComprar.push(item);
-        alert(mensaje);
-    });
-
-
-    let opcion = parseInt(prompt("Ingrese el número de la zapatilla elegida")); 
-    const calzadoElegido = filtro[opcion - 1];
-    if (opcion >= 1 && opcion <= filtro.length) {
-        alert(`Usted eligio ${calzadoElegido.marca}, ${calzadoElegido.modelo} `);
-    } else alert("Opción inválida.");
-    alert("Gracias por comprar tus zapatillas con nosotros. A continuación ingrese su email para ponernos en contacto con usted.")
-    prompt("Ingrese su email");
-    alert("Gracias por mandar su email, lo contactaremos lo mas pronto posible.")
-    return calzadoAComprar;
-};
-
-const venderCalzado = () => {
-    let vender = confirm("Quieres vender tus zapatillas?");
-    const cargarCalzado = [];
-
-    while (vender === true) {
-        cargarCalzado.push(new Zapatillas(prompt("Ingrese la marca"),
-            prompt("Ingrese el modelo de las zapatillas"),
-            parseInt(prompt("Ingrese el precio al que desea venderlo"))));
-        vender = confirm("Quieres vender algo más?");
-    };
-    
-    return cargarCalzado;
-};
-
-const concatDeCalzado = (calzadoPreCargado, calzadoCargadoPorElUsuario) => {
-    let concatDeCalzado = calzadoPreCargado.concat(calzadoCargadoPorElUsuario);
-    
-    if (calzadoCargadoPorElUsuario.length > 1) {
-        alert(`Gracias por vender ${calzadoCargadoPorElUsuario.length} tus zapatillas con nosotros. Ingrese a continuación su email para finalizar su venta.`);
-    } else if (calzadoCargadoPorElUsuario.length === 1) {
-        alert("Gracias por vender tus zapatillas. Ingrese a continuación su email para finalizar su venta.");
-        prompt("Ingrese su email");
-        alert("Gracias por mandar su email, lo contactaremos lo mas pronto posible.")
-    } else alert("Nos encantaria que vendas tus zapatillas con nosotros.");
-
-    return concatDeCalzado;
 }
 
-compraOVenta();
+const productos = [
+    new Producto("Superman El Nuevo Mundo", 5000, "./images/superman.webp"),
+    new Producto("Marvel Legacy Daredevil", 4200, "./images/dd.webp"),
+    new Producto("Spiderman", 7000, "./images/spiderman.webp"),
+    new Producto("Civil War parte 1", 5200, "./images/civilwar.webp"),
+    new Producto("Batman la broma asesina", 3800, "./images/comicbatman.webp"),
+    new Producto("DC Flash", 4000, "./images/flash.webp"),
+    new Producto("Deadpool Clasico Vol. 3", 4800, "./images/deadpool.webp"),
+    new Producto("Justice League Vol. 2", 6300, "./images/jl.webp"),
+    
+];
 
+const calcularTotal = (cantidad, precio) => cantidad * precio;
+
+const productosContainer = document.querySelector('.productos');
+const carritoLista = document.querySelector('.carrito-lista');
+const totalElement = document.getElementById('total');
+const finalizarCompraButton = document.getElementById('finalizar-compra');
+
+function mostrarProductos() {
+    productosContainer.innerHTML = productos.map((producto, index) => `
+        <div class="producto">
+            <img src="${producto.imagen}" alt="${producto.nombre}">
+            <span>${producto.nombre} - $${producto.precio}</span>
+            <input type="number" class="cantidad" min="1" value="1">
+            <button class="agregar-carrito" data-index="${index}">Agregar al carrito</button>
+        </div>
+    `).join('');
+}
+
+function actualizarCarrito() {
+    const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    carritoLista.innerHTML = carrito.map((item, index) => `
+        <li>
+            <img src="${item.imagen}" alt="${item.nombre}" class="miniatura">
+            ${item.nombre} - Cantidad: ${item.cantidad} - Subtotal: $${item.subtotal}
+            <button class="eliminar-producto" data-index="${index}">Eliminar</button>
+        </li>
+    `).join('');
+
+    const total = carrito.reduce((acc, item) => acc + item.subtotal, 0);
+    totalElement.textContent = total;
+}
+function agregarAlCarrito(event) {
+    if (event.target.classList.contains('agregar-carrito')) {
+        const index = parseInt(event.target.dataset.index);
+        const cantidad = parseInt(event.target.previousElementSibling.value);
+        const productoSeleccionado = productos[index];
+        const subtotal = calcularTotal(cantidad, productoSeleccionado.precio);
+
+        const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+        carrito.push({
+            nombre: productoSeleccionado.nombre,
+            cantidad: cantidad,
+            subtotal: subtotal,
+            imagen: productoSeleccionado.imagen
+        });
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+
+        actualizarCarrito();
+    }
+}
+
+function eliminarDelCarrito(event) {
+    if (event.target.classList.contains('eliminar-producto')) {
+        const index = parseInt(event.target.dataset.index);
+
+        const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+        carrito.splice(index, 1);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+
+        actualizarCarrito();
+    }
+}
+
+function finalizarCompra() {
+    localStorage.removeItem("carrito");
+    actualizarCarrito();
+    alert("Gracias por su compra, lo contactaremos a la brevedad.");
+}
+
+mostrarProductos();
+actualizarCarrito();
+productosContainer.addEventListener('click', agregarAlCarrito);
+carritoLista.addEventListener('click', eliminarDelCarrito);
+finalizarCompraButton.addEventListener('click', finalizarCompra);
 
  
